c20bc0596d489ffbc17aa77231fa334ee5ba3df4
Littlepandacai <caiamanda77@gmail.com>
2024-04-28 17:48:47 +0200

    flyttade på renderingrediencebox in i render app

--- logic/state.js
+++ logic/state.js
@@ -4,6 +4,10 @@ function render_App() {
     // Startsidan
     render_wrapper_DOM (); // startsidans wrapper anrop
     render_header(head); // header anrop
+
+//renderIngredientBox("wrapper");
+/* renderChosenIngredientsBox("wrapper"); */
+
     
 }
 
