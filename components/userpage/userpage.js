6696aebd9f5692daabe8ef5fb08b8df0a1296993
Littlepandacai <caiamanda77@gmail.com>
2024-05-14 10:41:54 +0200

    update

--- components/userpage/userpage.js
+++ components/userpage/userpage.js
@@ -1,3 +1,53 @@
-function renderUserpageContainer(parentID){
+function renderUserHeader(parentID){
+    const userHeader = document.createElement('header');
 
+    // Create userControl div
+    const userControlDiv = document.createElement('div');
+    userControlDiv.id = 'userControl';
+
+    // Create image element
+    const profileImg = document.createElement('img');
+    profileImg.src = 'defaultPfp.jpg';
+    profileImg.alt = 'default profile picture';
+    userControlDiv.appendChild(profileImg);
+
+    // Create buttons
+    const removeUserButton = document.createElement('button');
+    removeUserButton.textContent = 'Ta bort användare';
+    userControlDiv.appendChild(removeUserButton);
+
+    const logoutButton = document.createElement('button');
+    logoutButton.textContent = 'Logga ut';
+    userControlDiv.appendChild(logoutButton);
+
+    // Create userInfo div
+    const userInfoDiv = document.createElement('div');
+    userInfoDiv.id = 'userInfo';
+
+    // Create headings
+    const heading2 = document.createElement('h2');
+    heading2.textContent = 'My KITSCHen';
+    userInfoDiv.appendChild(heading2);
+
+    const heading3 = document.createElement('h3');
+    heading3.textContent = `${username} || ${email}`;
+    userInfoDiv.appendChild(heading3);
+
+    const heading4 = document.createElement('h4');
+    heading4.textContent = `${counter} saved recipes`;
+    userInfoDiv.appendChild(heading4);
+
+    // Append userControlDiv and userInfoDiv to userHeader
+    userHeader.appendChild(userControlDiv);
+    userHeader.appendChild(userInfoDiv);
+
+    
+
+    //lägger in i wrapper
+    document.querySelector(parentID).append(userHeader);
+}
+
+async function renderSavedRecipes(parentID){
+
+    
 }
---
+++ components/userpage/userpageContainer.js
@@ -0,0 +1,9 @@
+function renderUserpageContainer(parentID){
+
+    const DOM = document.getElementById('wrapper');
+    renderUserHeader(DOM);
+    // Hämta alla recept som innehåller ens user id baserat på (användarnamn, token)
+    
+    renderFooter();
+
+}
