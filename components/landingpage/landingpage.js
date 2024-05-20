e6aaf37193ab3cf54de305e4d8eea9eef5592216
michelleklara <michelleklara.kg@outlook.com>
2024-05-20 15:13:58 +0200

    test slogan

--- components/landingpage/landingpage.js
+++ components/landingpage/landingpage.js
@@ -47,7 +47,7 @@ function render_header (header) {
         <img src="./media/img/omslag1.png">
         <h1 class="title"> Kitsch </h1>
         
-        <div> TEXT SLOGAN  </div>
+        <div class="slogan_text"> Hitta recept av ingredienser du har hemma ....?</div>
         
         <div class="img_bottom">
 
