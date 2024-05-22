d034f6b2c174347e616e40bb4de360d3ce377c8a
michelleklara <michelleklara.kg@outlook.com>
2024-05-22 20:14:32 +0200

    test ny font och slogan

--- components/landingpage/landingpage.js
+++ components/landingpage/landingpage.js
@@ -47,7 +47,7 @@ function render_header (header) {
         <img src="./media/img/omslag1.png">
         <h1 class="title"> Kitsch </h1>
         
-        <div class="slogan_text"> <p>Hitta recept baserat på ingredienser du har hemma???</p></div>
+        <div class="slogan_text"> <p>Skapa smakupplevelser med dina ingredienser</p></div>
         
         <div class="img_bottom">
 
